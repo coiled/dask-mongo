cd769b44f82d9f16c815dc8b1a8f56668cbbb241
ncclementi <natyclementi@gmail.com>
2021-07-09 15:57:39 -0400

    start read mongo
    (cherry picked from commit cd761b69424f36b4a344c2ca485e277c679e375f)

--- dask_mongo/core.py
+++ dask_mongo/core.py
@@ -43,3 +43,59 @@ def to_mongo(
         dask.compute(partitions, **compute_options)
     else:
         return client.compute(partitions, **compute_options)
+
+
+def read_mongo(
+    connection_args: Dict,
+    database: str,
+    collection: str,
+    chunk_size: int,
+    match: Dict = {},
+):
+    with pymongo.MongoClient(**connection_args) as mongo_client:
+        db = mongo_client.get_database(database)
+
+        n_docs = next(
+            (
+                db[collection].aggregate(
+                    [
+                        {"$match": match},
+                        {"$count": "count"},
+                    ]
+                )
+            )
+        )["count"]
+
+        n_chunks = chunk_size // n_docs + bool(chunk_size % n_docs)
+
+        chunks_ids = list(
+            db["collection"].aggregate(
+                [
+                    {"$match": match},
+                    {"$bucketAuto": {"groupBy": "$_id", "buckets": n_chunks}},
+                ],
+                allowDiskUse=True,
+            )
+        )
+
+        chunks_list = []
+
+        # this is what we should do using dask compute
+        for chunk in chunks_ids:
+            id_min = chunk["_id"]["min"]
+            id_max = chunk["_id"]["max"]
+
+            chunks_list.append(
+                list(
+                    db["collection"].aggregate(
+                        [
+                            {"$match": match},
+                            {"$match": {"_id": {"$gte": id_min, "$lt": id_max}}},
+                        ]
+                    )
+                )
+            )
+
+        # not sure how to go about this part
+        # how to make a pd dataframe one partition on dask dataframe,
+        pd.DataFrame.from_records(chunks_list[0])
