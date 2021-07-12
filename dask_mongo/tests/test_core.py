07e0e241198a6babb3d8358e722934d792d33088
James Bourbeau <jrbourbeau@gmail.com>
2021-07-12 17:34:33 -0500

    Use client fixture

--- dask_mongo/tests/test_core.py
+++ dask_mongo/tests/test_core.py
@@ -5,8 +5,9 @@ import pandas as pd
 import pymongo
 import pytest
 from dask.dataframe.utils import assert_eq
-from distributed import Client, wait
-from distributed.utils_test import gen_cluster
+from distributed import wait
+from distributed.utils_test import cluster_fixture  # noqa: F401
+from distributed.utils_test import client, gen_cluster, loop  # noqa: F401
 
 from dask_mongo import read_mongo, to_mongo
 
@@ -81,92 +82,87 @@ def test_to_mongo_single_machine_scheduler(connection_args):
         assert_eq(ddf, result)
 
 
-def test_read_mongo(connection_args):
-    client = Client()  # noqa: F841
-
+def test_read_mongo(connection_args, client):
     df = pd.DataFrame({"a": range(10), "b": range(10, 20)})
     ddf = dd.from_pandas(df, npartitions=3)
 
-    with pymongo.MongoClient(**connection_args):
-        db_name = "test-db"
-        collection_name = "test-collection"
+    db_name = "test-db"
+    collection_name = "test-collection"
 
-        partitions = to_mongo(
-            ddf,
-            connection_args=connection_args,
-            database=db_name,
-            collection=collection_name,
-        )
+    partitions = to_mongo(
+        ddf,
+        connection_args=connection_args,
+        database=db_name,
+        collection=collection_name,
+    )
 
-        wait(partitions)
+    wait(partitions)
 
-        rm_ddf = read_mongo(
-            connection_args=connection_args,
-            database=db_name,
-            collection=collection_name,
-            chunksize=20,
-        )
+    rm_ddf = read_mongo(
+        connection_args=connection_args,
+        database=db_name,
+        collection=collection_name,
+        chunksize=20,
+    )
 
-        rm_ddf.compute()
+    rm_ddf.compute()
 
-        result = rm_ddf.drop(columns=["_id"]).sort_values(by="a").reset_index(drop=True)
+    result = rm_ddf.drop(columns=["_id"]).sort_values(by="a").reset_index(drop=True)
 
-        assert_eq(ddf, result, check_index=False, check_divisions=False)
+    assert_eq(ddf, result, check_index=False, check_divisions=False)
 
 
 def test_mongo_roundtrip_single_machine_scheduler(connection_args):
     df = pd.DataFrame({"a": range(10), "b": range(10, 20)})
     ddf = dd.from_pandas(df, npartitions=3)
 
-    with pymongo.MongoClient(**connection_args):
-        db_name = "test-db"
-        collection_name = "test-collection"
+    db_name = "test-db"
+    collection_name = "test-collection"
 
-        to_mongo(
-            ddf,
-            connection_args=connection_args,
-            database=db_name,
-            collection=collection_name,
-        )
+    to_mongo(
+        ddf,
+        connection_args=connection_args,
+        database=db_name,
+        collection=collection_name,
+    )
 
-        # read whole dataframe, match={} is default
-        rm_ddf = read_mongo(
-            connection_args=connection_args,
-            database=db_name,
-            collection=collection_name,
-            chunksize=20,
-        )
+    # read whole dataframe, match={} is default
+    rm_ddf = read_mongo(
+        connection_args=connection_args,
+        database=db_name,
+        collection=collection_name,
+        chunksize=20,
+    )
 
-        result = rm_ddf.drop(columns=["_id"]).sort_values(by="a").reset_index(drop=True)
+    result = rm_ddf.drop(columns=["_id"]).sort_values(by="a").reset_index(drop=True)
 
-        assert_eq(ddf, result, check_index=False, check_divisions=False)
+    assert_eq(ddf, result, check_index=False, check_divisions=False)
 
 
 def test_read_mongo_match(connection_args):
     df = pd.DataFrame({"a": range(10), "b": range(10, 20)})
     ddf = dd.from_pandas(df, npartitions=3)
 
-    with pymongo.MongoClient(**connection_args):
-        db_name = "test-db"
-        collection_name = "test-collection"
+    db_name = "test-db"
+    collection_name = "test-collection"
 
-        to_mongo(
-            ddf,
-            connection_args=connection_args,
-            database=db_name,
-            collection=collection_name,
-        )
+    to_mongo(
+        ddf,
+        connection_args=connection_args,
+        database=db_name,
+        collection=collection_name,
+    )
 
-        rm_ddf = read_mongo(
-            connection_args=connection_args,
-            database=db_name,
-            collection=collection_name,
-            chunksize=20,
-            match={"a": {"$gte": 2, "$lte": 7}},
-        )
+    rm_ddf = read_mongo(
+        connection_args=connection_args,
+        database=db_name,
+        collection=collection_name,
+        chunksize=20,
+        match={"a": {"$gte": 2, "$lte": 7}},
+    )
 
-        result = rm_ddf.drop(columns=["_id"]).sort_values(by="a").reset_index(drop=True)
+    result = rm_ddf.drop(columns=["_id"]).sort_values(by="a").reset_index(drop=True)
 
-        ddf_match = ddf[(ddf["a"] >= 2) & (ddf["a"] <= 7)]
+    ddf_match = ddf[(ddf["a"] >= 2) & (ddf["a"] <= 7)]
 
-        assert_eq(ddf_match, result, check_index=False, check_divisions=False)
+    assert_eq(ddf_match, result, check_index=False, check_divisions=False)
