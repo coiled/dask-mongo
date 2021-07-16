eb591d564734e2d5de281420b319e4c1469f7fbb
crusaderky <crusaderky@gmail.com>
2021-07-16 17:12:21 +0100

    compute=True

--- dask_mongo/core.py
+++ dask_mongo/core.py
@@ -1,22 +1,22 @@
 from __future__ import annotations
 
 from math import ceil
+from typing import Any
 
 import pymongo
 from bson import ObjectId
 from dask.bag import Bag
 from dask.base import tokenize
-from dask.delayed import Delayed
 from dask.graph_manipulation import checkpoint
 
 
 def write_mongo(
     values: list[dict],
-    connection_args: dict,
+    connection_kwargs: dict[str, Any],
     database: str,
     collection: str,
 ) -> None:
-    with pymongo.MongoClient(**connection_args) as mongo_client:
+    with pymongo.MongoClient(**connection_kwargs) as mongo_client:
         coll = mongo_client[database][collection]
         # `insert_many` will mutate its input by inserting a "_id" entry.
         # This can lead to confusing results; pass copies to it to preserve the input.
@@ -26,41 +26,56 @@ def write_mongo(
 
 def to_mongo(
     bag: Bag,
-    connection_args: dict,
     database: str,
     collection: str,
-) -> Delayed:
+    *,
+    connection_kwargs: dict[str, Any] = None,
+    compute: bool = True,
+    compute_kwargs: dict[str, Any] = None,
+) -> Any:
     """Write a Dask Bag to a Mongo database.
 
     Parameters
     ----------
     bag:
       Dask Bag to write into the database.
-    connection_args:
-      Connection arguments to pass to ``MongoClient``.
-    database:
+    database : str
       Name of the database to write to. If it does not exists it will be created.
-    collection:
+    collection : str
       Name of the collection within the database to write to.
       If it does not exists it will be created.
+    connection_kwargs : dict
+      Arguments to pass to ``MongoClient``.
+    compute : bool, optional
+        If true, immediately executes. If False, returns a delayed
+        object, which can be computed at a later time.
+    compute_kwargs : dict, optional
+        Options to be passed in to the compute method
     Returns
     -------
-    dask.delayed
+    If compute=True, block until computation is done, then return None.
+    If compute=False, immediately return a dask.delayed object.
     """
-    partials = bag.map_partitions(write_mongo, connection_args, database, collection)
-    return checkpoint(partials)
+    partials = bag.map_partitions(
+        write_mongo, connection_kwargs or {}, database, collection
+    )
+    collect = checkpoint(partials)
+    if compute:
+        return collect.compute(**compute_kwargs or {})
+    else:
+        return collect
 
 
 def fetch_mongo(
-    connection_args: dict,
+    connection_kwargs: dict[str, Any],
     database: str,
     collection: str,
-    match: dict,
+    match: dict[str, Any],
     id_min: ObjectId,
     id_max: ObjectId,
     include_last: bool,
-):
-    with pymongo.MongoClient(**connection_args) as mongo_client:
+) -> list[dict[str, Any]]:
+    with pymongo.MongoClient(**connection_kwargs) as mongo_client:
         coll = mongo_client[database][collection]
         return list(
             coll.aggregate(
@@ -80,32 +95,35 @@ def fetch_mongo(
 
 
 def read_mongo(
-    connection_args: dict,
     database: str,
     collection: str,
     chunksize: int,
-    match: dict = None,
+    *,
+    connection_kwargs: dict[str, Any] = None,
+    match: dict[str, Any] = None,
 ):
     """Read data from a Mongo database into a Dask Bag.
 
     Parameters
     ----------
-    connection_args:
-      Connection arguments to pass to ``MongoClient``
     database:
       Name of the database to read from
     collection:
       Name of the collection within the database to read from
     chunksize:
       Number of elements desired per partition.
+    connection_kwargs:
+      Connection arguments to pass to ``MongoClient``
     match:
       MongoDB match query, used to filter the documents in the collection. If omitted,
       this function will load all the documents in the collection.
     """
+    if not connection_kwargs:
+        connection_kwargs = {}
     if not match:
         match = {}
 
-    with pymongo.MongoClient(**connection_args) as mongo_client:
+    with pymongo.MongoClient(**connection_kwargs) as mongo_client:
         coll = mongo_client[database][collection]
 
         nrows = next(
@@ -131,7 +149,7 @@ def read_mongo(
             )
         )
 
-    common_args = (connection_args, database, collection, match)
+    common_args = (connection_kwargs, database, collection, match)
     name = "read_mongo-" + tokenize(common_args, chunksize)
     dsk = {
         (name, i): (
--- dask_mongo/tests/test_core.py
+++ dask_mongo/tests/test_core.py
@@ -13,16 +13,16 @@ from dask_mongo import read_mongo, to_mongo
 
 
 @pytest.fixture
-def connection_args(tmp_path):
+def connection_kwargs(tmp_path):
     port = 27016
     with subprocess.Popen(
         ["mongod", f"--dbpath={str(tmp_path)}", f"--port={port}"]
     ) as proc:
-        connection_args = {
+        connection_kwargs = {
             "host": "localhost",
             "port": port,
         }
-        yield connection_args
+        yield connection_kwargs
         proc.terminate()
 
 
@@ -39,7 +39,7 @@ def gen_data(size=10):
 
 
 @gen_cluster(client=True, clean_kwargs={"threads": False})
-async def test_to_mongo_distributed(c, s, a, b, connection_args):
+async def test_to_mongo_distributed_async(c, s, a, b, connection_kwargs):
     records = gen_data(size=10)
     npartitions = 3
     b = db.from_sequence(records, npartitions=npartitions)
@@ -48,12 +48,14 @@ async def test_to_mongo_distributed(c, s, a, b, connection_args):
     collection = "test-collection"
     delayed_ = to_mongo(
         b,
-        connection_args=connection_args,
-        database=database,
-        collection=collection,
+        database,
+        collection,
+        connection_kwargs=connection_kwargs,
+        # DaskMethodsMixin.compute() does not work with async distributed.Client
+        compute=False,
     )
 
-    with pymongo.MongoClient(**connection_args) as mongo_client:
+    with pymongo.MongoClient(**connection_kwargs) as mongo_client:
         assert database not in mongo_client.list_database_names()
         await c.compute(delayed_)
         assert database in mongo_client.list_database_names()
@@ -66,22 +68,37 @@ async def test_to_mongo_distributed(c, s, a, b, connection_args):
     assert_eq(b, results)
 
 
-def test_to_mongo(connection_args):
+@pytest.mark.parametrize(
+    "compute,compute_kwargs",
+    [
+        (False, None),
+        (True, None),
+        (True, dict(scheduler="sync")),
+    ],
+)
+def test_to_mongo(connection_kwargs, compute, compute_kwargs):
     records = gen_data(size=10)
     npartitions = 3
     b = db.from_sequence(records, npartitions=npartitions)
     database = "test-db"
     collection = "test-collection"
-    delayed_ = to_mongo(
-        b,
-        connection_args=connection_args,
-        database=database,
-        collection=collection,
-    )
 
-    with pymongo.MongoClient(**connection_args) as mongo_client:
+    with pymongo.MongoClient(**connection_kwargs) as mongo_client:
         assert database not in mongo_client.list_database_names()
-        delayed_.compute()
+
+        out = to_mongo(
+            b,
+            database,
+            collection,
+            connection_kwargs=connection_kwargs,
+            compute=compute,
+            compute_kwargs=compute_kwargs,
+        )
+        if compute:
+            assert out is None
+        else:
+            assert out.compute() is None
+
         assert database in mongo_client.list_database_names()
         assert [collection] == mongo_client[database].list_collection_names()
 
@@ -92,40 +109,40 @@ def test_to_mongo(connection_args):
     assert_eq(b, results)
 
 
-def test_read_mongo(connection_args, client):
+def test_read_mongo(connection_kwargs, client):
     records = gen_data(size=10)
     database = "test-db"
     collection = "test-collection"
 
-    with pymongo.MongoClient(**connection_args) as mongo_client:
+    with pymongo.MongoClient(**connection_kwargs) as mongo_client:
         database_ = mongo_client.get_database(database)
         database_[collection].insert_many(deepcopy(records))
 
     b = read_mongo(
-        connection_args=connection_args,
-        database=database,
-        collection=collection,
+        database,
+        collection,
         chunksize=5,
+        connection_kwargs=connection_kwargs,
     )
 
     b = b.map(lambda x: {k: v for k, v in x.items() if k != "_id"})
     assert_eq(b, records)
 
 
-def test_read_mongo_match(connection_args):
+def test_read_mongo_match(connection_kwargs):
     records = gen_data(size=10)
     database = "test-db"
     collection = "test-collection"
 
-    with pymongo.MongoClient(**connection_args) as mongo_client:
+    with pymongo.MongoClient(**connection_kwargs) as mongo_client:
         database_ = mongo_client.get_database(database)
         database_[collection].insert_many(deepcopy(records))
 
     b = read_mongo(
-        connection_args=connection_args,
-        database=database,
-        collection=collection,
+        database,
+        collection,
         chunksize=5,
+        connection_kwargs=connection_kwargs,
         match={"idx": {"$gte": 2, "$lte": 7}},
     )
 
@@ -134,21 +151,21 @@ def test_read_mongo_match(connection_args):
     assert_eq(b, expected)
 
 
-def test_read_mongo_chunksize(connection_args):
+def test_read_mongo_chunksize(connection_kwargs):
     records = gen_data(size=10)
     database = "test-db"
     collection = "test-collection"
 
-    with pymongo.MongoClient(**connection_args) as mongo_client:
+    with pymongo.MongoClient(**connection_kwargs) as mongo_client:
         database_ = mongo_client.get_database(database)
         database_[collection].insert_many(deepcopy(records))
 
     # divides evenly total nrows, 10/5 = 2
     b = read_mongo(
-        connection_args=connection_args,
-        database=database,
-        collection=collection,
+        database,
+        collection,
         chunksize=5,
+        connection_kwargs=connection_kwargs,
     )
 
     assert b.npartitions == 2
@@ -156,10 +173,10 @@ def test_read_mongo_chunksize(connection_args):
 
     # does not divides evenly total nrows, 10/3 -> 4
     b = read_mongo(
-        connection_args=connection_args,
-        database=database,
-        collection=collection,
+        database,
+        collection,
         chunksize=3,
+        connection_kwargs=connection_kwargs,
     )
 
     assert b.npartitions == 4
