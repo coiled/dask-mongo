441c600506e4f033b66db92ee8524b47aa17cb9a
ncclementi <natyclementi@gmail.com>
2022-05-25 15:41:46 -0400

    Release bump 2022.5.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="dask-mongo",
-    version="2021.7.3",
+    version="2022.5.0",
     description="Dask + Mongo intergration",
     license="BSD",
     packages=["dask_mongo"],
